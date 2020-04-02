6b63bba394657df73041cd153f21a732165847b9
peaBerberian <pea.berberian@gmail.com>
2020-04-02 18:57:38 +0200

    init/fetchers/transports: add the concept of unsafeMode to Manifest fetching

--- src/core/init/initialize_media_source.ts
+++ src/core/init/initialize_media_source.ts
@@ -56,6 +56,7 @@ import {
 import {
   createManifestFetcher,
   IManifestFetcherParsedResult,
+  IManifestFetcherParserOptions,
   SegmentFetcherCreator,
 } from "../fetchers";
 import { ITextTrackSourceBufferOptions } from "../source_buffers";
@@ -190,13 +191,13 @@ export default function InitializeOnMediaSource(
    * Fetch and parse the manifest from the URL given.
    * Throttled to avoid doing multiple simultaneous requests.
    */
-  const fetchManifest = throttle((manifestURL? : string,
-                                  externalClockOffset? : number)
+  const fetchManifest = throttle((manifestURL : string | undefined,
+                                  options : IManifestFetcherParserOptions)
     : Observable<IWarningEvent | IManifestFetcherParsedResult> =>
       manifestFetcher.fetch(manifestURL).pipe(
         mergeMap((response) => response.type === "warning" ?
           observableOf(response) : // bubble-up warnings
-          response.parse({ externalClockOffset })),
+          response.parse(options)),
         share()));
 
   /** Interface used to download segments. */
@@ -240,7 +241,8 @@ export default function InitializeOnMediaSource(
                                             mapTo(true));
 
   /** Do the first Manifest request. */
-  const initialManifestRequest$ = fetchManifest(url, undefined).pipe(
+  const initialManifestRequest$ = fetchManifest(url, { previousManifest: null,
+                                                       unsafeMode: false }).pipe(
     subscribeOn(asapScheduler), // to launch subscriptions only when all
     share());                   // Observables here are linked
 
